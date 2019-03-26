fc1f7fcd1aa21cce78880c7bc1b596aea722dd2e
Omar Houmz <omar.houmz@schibsted.com>
2019-03-26 21:18:49 +0100

    add static items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import { items } from "./data";
+import "./app.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="app">
+        <ul className="item-list">
+          {items.map(item => (
+            <li className="item">
+              <img
+                className="item-image"
+                alt=""
+                src="https://picsum.photos/200/300"
+              />
+              <span className="item-title">{item.title}</span>
+              <span className="item-city">{item.city}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const CITIES_LIST = ["Casablanca", "Rabat", "Tangier", "Agadir"];
+
+export const items = [
+  {
+    title: "This is a good title",
+    city: "Casablanca"
+  },
+  {
+    title: "This is another title",
+    city: "Tangier"
+  },
+  {
+    title: "Molestias quasi exercitationem",
+    city: "Agadir"
+  }
+];
