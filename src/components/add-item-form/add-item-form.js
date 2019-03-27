51d101adfd8a21fead44747ca5b5a9b037c950b8
Omar Houmz <omar.houmz@schibsted.com>
2019-03-27 23:33:34 +0100

    add image field in add item form

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -6,14 +6,18 @@ import "./add-item-form.css";
 export class AddItemForm extends React.PureComponent {
   onSubmit = event => {
     event.preventDefault();
-    const title = event.target[0].value;
-    const city = event.target[1].value;
+    const title = event.target.title.value;
+    const city = event.target.city.value;
+    const image = event.target.image.value;
 
-    if (title === "" || city === "") {
+    if (title === "" || city === "" || image === "") {
+      alert("Fill all fields!");
       return;
     }
 
-    event.target[0].value = "";
+    event.target.title.value = "";
+    event.target.city.value = "";
+    event.target.image.value = "";
 
     this.props.onSubmit({ title, city });
   };
@@ -23,13 +27,20 @@ export class AddItemForm extends React.PureComponent {
       <form className="add-item-form" onSubmit={this.onSubmit}>
         <h2 className="heading">Add an item:</h2>
         <div className="data-inputs">
+          <label htmlFor="image">
+            Image:
+            <input type="file" name="image" />
+          </label>
           <label htmlFor="title">
             Title:
-            <input type="text" id="title" />
+            <input type="text" name="title" />
           </label>
           <label htmlFor="city">
             City:
-            <select id="city">
+            <select name="city" defaultValue="">
+              <option value="" disabled>
+                Choose a city
+              </option>
               {CITIES_LIST.map(city => (
                 <option key={city} value={city}>
                   {city}
