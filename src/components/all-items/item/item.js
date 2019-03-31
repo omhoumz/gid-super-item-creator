048a6c1604734925a1d0cd2a99ba44b81e84ec32
Omar Houmz <omar.houmz@schibsted.com>
2019-03-31 01:06:14 +0100

    update item with id and delete onClick function

--- src/components/all-items/item/item.js
+++ src/components/all-items/item/item.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./item.css";
 
-export const Item = ({ title, city }) => {
+export const Item = ({ id, title, city, image, onDelete }) => {
   return (
     <li className="item">
-      <img className="item-image" alt="" src="https://picsum.photos/200/300" />
+      <img className="item-image" alt="" src={image} />
       <span className="item-title">{title}</span>
       <span className="item-city">{city}</span>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </li>
   );
 };
