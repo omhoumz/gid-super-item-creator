c7692816b066916891d8a352739161ecc876721e
Omar Houmz <omar.houmz@schibsted.com>
2019-03-31 21:57:12 +0100

    item-form to a controlled form

--- src/components/item-form/item-form.js
+++ src/components/item-form/item-form.js
@@ -4,11 +4,28 @@ import { CITIES_LIST } from "../../db/data";
 import "./item-form.css";
 
 export class ItemForm extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      city: ""
+    };
+
+    this.fileInput = React.createRef();
+  }
+
+  onChange = event => {
+    const key = event.target.name;
+    const value = event.target.type !== "file" && event.target.value;
+
+    this.setState({ [key]: value });
+  };
+
   onSubmit = event => {
     event.preventDefault();
-    const title = event.target.title.value;
-    const city = event.target.city.value;
-    const imageFile = event.target.image.files[0];
+
+    const { title, city } = this.state;
+    const imageFile = this.fileInput.current.files[0];
 
     if (title === "" || city === "" || !imageFile) {
       alert("Fill all fields!");
@@ -19,11 +36,15 @@ export class ItemForm extends React.PureComponent {
     event.target.city.value = "";
     event.target.image.value = "";
 
+    this.setState({ title: "", city: "" });
+
     this.props.onSubmit({ title, city, imageFile });
   };
 
   render() {
     const { buttonLabel, heading } = this.props;
+
+    const { title, city } = this.state;
 
     return (
       <form className="item-form" onSubmit={this.onSubmit}>
@@ -31,15 +52,20 @@ export class ItemForm extends React.PureComponent {
         <div className="data-inputs">
           <label htmlFor="image">
             Image:
-            <input type="file" name="image" />
+            <input type="file" name="image" ref={this.fileInput} />
           </label>
           <label htmlFor="title">
             Title:
-            <input type="text" name="title" />
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.onChange}
+            />
           </label>
           <label htmlFor="city">
             City:
-            <select name="city" defaultValue="">
+            <select name="city" value={city} onChange={this.onChange}>
               <option value="" disabled>
                 Choose a city
               </option>
